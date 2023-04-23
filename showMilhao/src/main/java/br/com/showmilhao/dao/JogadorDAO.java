bed5a2b2952257cdee0b45afd8c90b8bf54c230a
RaulMCFidelis <raul.mcfidelis@senacsp.edu.br>
2023-04-23 01:01:52 -0300

    JogadorDAO
    Adicionar novos jogadores ao Banco de Dados.

---
+++ showMilhao/src/main/java/br/com/showmilhao/dao/JogadorDAO.java
@@ -0,0 +1,34 @@
+package br.com.showmilhao.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import br.com.showmilhao.connection.ConnectionFactory;
+import br.com.showmilhao.model.Jogador;
+import br.com.showmilhao.util.LogUtil;
+
+public class JogadorDAO {
+	
+	private Connection connection;
+	
+	public JogadorDAO() {
+		connection = ConnectionFactory.getConnection();		
+	}
+
+	public boolean adicionar(Jogador jogador) {
+		try {
+			String sql = "INSERT INTO jogador (id, nome, pontuacao) VALUES ($next_id, ?, ?)";
+			try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+				stmt.setString(2, jogador.getNome());
+				stmt.setInt(3, jogador.getPontuacao());
+				stmt.executeUpdate();
+				connection.commit();
+				return true;
+			}
+		} catch (Exception e) {
+			LogUtil.getLogger(JogadorDAO.class).error(e.getCause().toString());
+			return false;
+		}		
+	}
+	
+}
